559f56511afe1cf5975bd8f4604b93161f03be70
dell <abhishek11sgh@gmail.com>
2026-06-08 16:23:24 +0530

    cors added

--- Server.js
+++ Server.js
@@ -10,7 +10,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://job-portal-frontend-xi-five.vercel.app/",
 }));
 
 app.use(express.json());
